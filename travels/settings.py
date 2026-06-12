c3e0bc4d570f6c72b737a4cbf7a81a830c32d009
Chandrashekar <your.email@gmail.com>
2026-06-12 13:33:26 +0530

    updated

--- travels/settings.py
+++ travels/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2h=wy+tu$7og4+#+chw&)(h_8_iaa+77i!j($g^=zwetduud^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'bus-travel-api.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
